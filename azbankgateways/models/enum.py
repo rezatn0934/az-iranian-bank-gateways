fb02a03208bf5153d55e9f662d3624cd329ac12e
rezatn0934 <rezatn0934@gmail.com>
2024-12-15 23:40:35 +0330

    :hammer: add TOP to BankType

--- azbankgateways/models/enum.py
+++ azbankgateways/models/enum.py
@@ -11,6 +11,7 @@ class BankType(models.TextChoices):
     BAHAMTA = "BAHAMTA", _("Bahamta")
     MELLAT = "MELLAT", _("Mellat")
     PAYV1 = "PAYV1", _("PayV1")
+    TOP = "TOP", _("Top")
 
 
 class CurrencyEnum(models.TextChoices):
