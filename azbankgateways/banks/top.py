68292a591bfea59e274523033f5d588be3a48343
rezatn0934 <rezatn0934@gmail.com>
2024-12-17 14:17:28 +0330

    :hammer: update Top

--- azbankgateways/banks/top.py
+++ azbankgateways/banks/top.py
@@ -2,6 +2,7 @@ import logging
 from json import dumps, loads
 from time import gmtime, strftime
 
+from django.utils.translation import gettext_lazy as _
 from zeep import Client, Transport
 
 from azbankgateways.banks import BaseBank
@@ -18,7 +19,7 @@ class Top(BaseBank):
     def __init__(self, **kwargs):
         super(Top, self).__init__(**kwargs)
         self.set_gateway_currency(CurrencyEnum.IRR)
-        self._payment_url = "https://pec.shaparak.ir/NewIPGServices/Sale/SaleService.asmx"
+        self._payment_url = "https://pec.shaparak.ir/NewIPG/"
 
     def get_bank_type(self):
         return BankType.TOP
@@ -48,7 +49,7 @@ class Top(BaseBank):
         return params
 
     def _get_gateway_payment_method_parameter(self):
-        return "GET"
+        return "POST"
 
     """
     pay
@@ -57,16 +58,12 @@ class Top(BaseBank):
     def get_pay_data(self):
         description = "خرید با شماره پیگیری - {}".format(self.get_tracking_code())
         data = {
-            "terminalId": int(self._terminal_code),
-            "userName": self._username,
-            "userPassword": self._password,
+            "LoginAccount": self._password,
             "orderId": int(self.get_tracking_code()),
-            "amount": int(self.get_gateway_amount()),
-            "localDate": self._get_current_date(),
-            "localTime": self._get_current_time(),
-            "additionalData": description,
-            "callBackUrl": self._get_gateway_callback_url(),
-            "payerId": 0,
+            "Amount": int(self.get_gateway_amount()),
+            "CallBackUrl": self._get_gateway_callback_url(),
+            "AdditionalData": description,
+            "Originator": self.get_mobile_number(),
         }
         return data
 
@@ -78,104 +75,13 @@ class Top(BaseBank):
 
         data = self.get_pay_data()
         client = self._get_client()
-        response = client.service.bpPayRequest(**data)
+        response = client.service.SalePayment(**data)
         try:
             status, token = response.split(",")
             if status == "0":
                 self._set_reference_number(token)
         except ValueError:
-            status_text = "Unknown error"
-            if response == "11":
-                status_text = "Card number is invalid"
-            elif response == "12":
-                status_text = "Insufficient inventory"
-            elif response == "13":
-                status_text = "Password is incorrect"
-            elif response == "14":
-                status_text = "Max try reached"
-            elif response == "15":
-                status_text = "Card is invalid"
-            elif response == "16":
-                status_text = "The number of withdrawals is more than allowed"
-            elif response == "17":
-                status_text = "The user has abandoned the transaction"
-            elif response == "18":
-                status_text = "The card has expired"
-            elif response == "19":
-                status_text = "The withdrawal amount is over the limit"
-            elif response == "21":
-                status_text = "Invalid service"
-            elif response == "23":
-                status_text = "A security error has occurred"
-            elif response == "24":
-                status_text = "The recipient's user information is invalid"
-            elif response == "25":
-                status_text = "The amount is invalid"
-            elif response == "31":
-                status_text = "The response is invalid"
-            elif response == "32":
-                status_text = "The format of the entered information is not correct"
-            elif response == "33":
-                status_text = "The account is invalid"
-            elif response == "34":
-                status_text = "System error"
-            elif response == "35":
-                status_text = "Date is invalid"
-            elif response == "41":
-                status_text = "The request number is duplicate"
-            elif response == "42":
-                status_text = "Sale transaction not found"
-            elif response == "43":
-                status_text = "Verify has already been requested"
-            elif response == "44":
-                status_text = "Verify request not found"
-            elif response == "45":
-                status_text = "The transaction has been settled"
-            elif response == "46":
-                status_text = "The transaction has not been settled"
-            elif response == "47":
-                status_text = "Settle transaction not found"
-            elif response == "48":
-                status_text = "The transaction has been reversed"
-            elif response == "49":
-                status_text = "Refund transaction not found"
-            elif response == "51":
-                status_text = "The transaction is repeated"
-            elif response == "54":
-                status_text = "The reference transaction does not exist"
-            elif response == "55":
-                status_text = "The transaction is invalid"
-            elif response == "61":
-                status_text = "Error in deposit"
-            elif response == "111":
-                status_text = "Card issuer is invalid"
-            elif response == "112":
-                status_text = "Card issuing switch error"
-            elif response == "113":
-                status_text = "No response was received from the card issuer"
-            elif response == "114":
-                status_text = "The cardholder is not authorized to perform this transaction"
-            elif response == "113":
-                status_text = "No response was received from the card issuer"
-            elif response == "412":
-                status_text = "The invoice ID is incorrect"
-            elif response == "413":
-                status_text = "Payment ID is incorrect"
-            elif response == "414":
-                status_text = "The organization issuing the bill is invalid"
-            elif response == "415":
-                status_text = "The working session has ended"
-            elif response == "416":
-                status_text = "The working session has ended"
-            elif response == "417":
-                status_text = "Payer ID is invalid"
-            elif response == "418":
-                status_text = "Problems in defining customer information"
-            elif response == "419":
-                status_text = "The number of data entries has exceeded the limit"
-            elif response == "421":
-                status_text = "Invalid IP address"
-
+            status_text = self._get_status_text(response=response)
             self._set_transaction_status_text(status_text)
             logging.critical(status_text)
             raise BankGatewayRejectPayment(self.get_transaction_status_text())
@@ -187,12 +93,12 @@ class Top(BaseBank):
     def prepare_verify_from_gateway(self):
         super(Top, self).prepare_verify_from_gateway()
         post = self.get_request().POST
-        token = post.get("RefId", None)
+        token = post.get("RefId")
         if not token:
             return
         self._set_reference_number(token)
         self._set_bank_record()
-        self._bank.extra_information = dumps(dict(zip(post.keys(), post.values())))
+        self._bank.extra_information = dumps(dict(post.items()))
         self._bank.save()
 
     def verify_from_gateway(self, request):
@@ -251,7 +157,9 @@ class Top(BaseBank):
     @staticmethod
     def _get_client():
         transport = Transport(timeout=5, operation_timeout=5)
-        client = Client("https://pec.shaparak.ir/NewIPGServices/Sale/SaleService.asmx?wsdl", transport=transport)
+        client = Client(
+            "https://pec.shaparak.ir/NewIPGServices/Sale/SaleService.asmx?wsdl", transport=transport
+        )
         return client
 
     @staticmethod
@@ -261,6 +169,106 @@ class Top(BaseBank):
     @staticmethod
     def _get_current_date():
         return strftime("%Y%m%d", gmtime())
+
+    @staticmethod
+    def _get_status_text(response):
+        status_text = {
+            "-1552": _("Payment Request Is Not Eligible To Reversal"),
+            "-1551": _("Payment Request Is Already Reversed"),
+            "-1550": _("Payment Request Status Is Not Reversible"),
+            "-1549": _("Max Allowed Time To Reversal Has Exceeded"),
+            "-1548": _("Bill Payment Request Service Failed"),
+            "-1540": _("Invalid Confirm Request Service"),
+            "-1536": _("Topup Charge Service Topup Charge Request Failed"),
+            "-1533": _("Payment Is Already Confirmed"),
+            "-1532": _("Merchant Has Confirmed Payment Request"),
+            "-1531": _("Cannot Confirm NonSuccessful Payment"),
+            "-1530": _("Merchant Confirm Payment Request Access Violated"),
+            "-1528": _("Confirm Payment Request Info Not Found"),
+            "-1527": _("Call Sale Payment Request Service Failed"),
+            "-1507": _("Reversal Completed"),
+            "-1505": _("Payment Confirm Requested"),
+            "-138": _("Canceled By User"),
+            "-132": _("Invalid Minimum Payment Amount"),
+            "-131": _("Invalid Token"),
+            "-130": _("Token Is Expired"),
+            "-128": _("Invalid Ip Address Format"),
+            "-127": _("Invalid Merchant Ip"),
+            "-126": _("Invalid Merchant Pin"),
+            "-121": _("Invalid String Is Numeric"),
+            "-120": _("Invalid Length"),
+            "-119": _("Invalid Organization Id"),
+            "-118": _("Value Is Not Numeric"),
+            "-117": _("Length Is Less Of Minimum"),
+            "-116": _("Length Is More Of Maximum"),
+            "-115": _("Invalid Pay Id"),
+            "-114": _("Invalid Bill Id"),
+            "-113": _("Value Is Null"),
+            "-112": _("Order Id Duplicated"),
+            "-111": _("Invalid Merchant Max Trans Amount"),
+            "-108": _("Reverse Is Not Enabled"),
+            "-107": _("Advice Is Not Enabled"),
+            "-106": _("Charge Is Not Enabled"),
+            "-105": _("Topup Is Not Enabled"),
+            "-104": _("Bill Is Not Enabled"),
+            "-103": _("Sale Is Not Enabled"),
+            "-102": _("Reverse Successful"),
+            "-101": _("Merchant Authentication Failed"),
+            "-100": _("Merchant Is Not Active"),
+            "-1": _("Server Error"),
+            "0": _("Successful"),
+            "1": _("Refer To Card Issuer Decline"),
+            "2": _("Refer To Card Issuer Special Conditions"),
+            "3": _("Invalid Merchant"),
+            "5": _("Do Not Honour"),
+            "6": _("Error"),
+            "8": _("Honour With Identification"),
+            "9": _("Request In Progress"),
+            "10": _("Approved For Partial Amount"),
+            "12": _("Invalid Transaction"),
+            "13": _("Invalid Amount"),
+            "14": _("Invalid Card Number"),
+            "15": _("No Such Issuer"),
+            "17": _("Customer Cancellation"),
+            "20": _("Invalid Response"),
+            "21": _("No Action Taken"),
+            "22": _("Suspected Malfunction"),
+            "30": _("Format Error"),
+            "31": _("Bank Not Supported By Switch"),
+            "32": _("Completed Partially"),
+            "33": _("Expired Card Pick Up"),
+            "38": _("Allowable PIN Tries Exceeded Pick Up"),
+            "39": _("No Credit Account"),
+            "40": _("Requested Function is not Supported"),
+            "41": _("Lost Card"),
+            "43": _("Stolen Card"),
+            "45": _("Bill Can not Be Payed"),
+            "51": _("No Sufficient Funds"),
+            "54": _("Expired Account"),
+            "55": _("Incorrect PIN"),
+            "56": _("No Card Record"),
+            "57": _("Transaction Not Permitted To CardHolder"),
+            "58": _("Transaction Not Permitted To Terminal"),
+            "59": _("Suspected Fraud-Decline"),
+            "61": _("Exceeds Withdrawal Amount Limit"),
+            "62": _("Restricted Card-Decline"),
+            "63": _("Security Violation"),
+            "65": _("Exceeds Withdrawal Frequency Limit"),
+            "68": _("Response Received Too Late"),
+            "69": _("Allowable Number Of PIN Tries Exceeded"),
+            "75": _("PIN Reties Exceeds-Slm"),
+            "78": _("Deactivated Card-Slm"),
+            "79": _("Invalid Amount-Slm"),
+            "80": _("Transaction Denied-Slm"),
+            "81": _("Cancelled Card-Slm"),
+            "83": _("Host Refuse-Slm"),
+            "84": _("Issuer Down-Slm"),
+            "91": _("Issuer Or Switch Is Inoperative"),
+            "92": _("Financial Inst Or Intermediate Net Facility Not Found for Routing"),
+            "93": _("Transaction Cannot Be Completed"),
+        }
+
+        return status_text.get(response, _("Unknown error"))
 
     def _get_sale_reference_id(self):
         extra_information = loads(getattr(self._bank, "extra_information", "{}"))
